6ba71394e71f531644854677754fe0f12c708a74
Ashureth-dev <133978019+Ashureth-dev@users.noreply.github.com>
2024-03-02 02:44:28 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const Eris = require("eris");
+/*const Eris = require("eris");
 const keep_alive = require('./keep_alive.js')
 
 // Replace TOKEN with your bot account's token
@@ -8,4 +8,17 @@ bot.on("error", (err) => {
   console.error(err); // or your preferred logger
 });
 
-bot.connect(); // Get the bot to connect to Discord
+bot.connect(); // Get the bot to connect to Discord*/
+const keep_alive = require('./keep_alive.js')
+const { Client } = require('discord.js-selfbot-v13');
+const client = new Client();
+
+client.on('ready', async () => {
+  console.log(`${client.user.username} is ready!`);
+})
+
+client.on('error', (err) => {
+  console.error(err);
+});
+
+client.login('token');
